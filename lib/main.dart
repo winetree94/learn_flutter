4d12cdc2468a5ae174c0e6f2e9a6c82aec81c477
winetree94 <me@winetree94.com>
2026-07-01 13:33:39 +0900

    a

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'game.dart';
+import 'package:learn_flutter/game.dart';
+import 'package:learn_flutter/tile.dart';
 
 void main() {
   runApp(const MainApp());
@@ -13,9 +14,39 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Tile(letter: 'A', hitType: HitType.partial),
+        appBar: AppBar(
+          title: Align(alignment: .centerLeft, child: Text('Birdle')),
         ),
+        body: Center(child: GamePage()),
+      ),
+    );
+  }
+}
+
+class GamePage extends StatelessWidget {
+  GamePage({super.key});
+
+  final Game _game = Game();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        spacing: 5.0,
+        children: [
+          for (final guess in _game.guesses)
+            Row(
+              spacing: 5.0,
+              children: [
+                for (final col in guess)
+                  Column(
+                    spacing: 5.0,
+                    children: [Tile(letter: col.char, hitType: col.type)],
+                  ),
+              ],
+            ),
+        ],
       ),
     );
   }
